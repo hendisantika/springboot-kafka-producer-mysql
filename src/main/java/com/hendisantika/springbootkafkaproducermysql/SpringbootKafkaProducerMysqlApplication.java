75a687307f39f4794a99ab66e05fc73fcc29af8c
Hendi Santika <hendisantika@gmail.com>
2021-05-10 04:55:48 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootkafkaproducermysql/SpringbootKafkaProducerMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootkafkaproducermysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProducerMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaProducerMysqlApplication.class, args);
+    }
+
+}
