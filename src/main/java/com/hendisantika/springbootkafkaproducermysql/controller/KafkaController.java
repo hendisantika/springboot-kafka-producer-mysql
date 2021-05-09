e991d43f65e6200db373686221aee7d3a4e139b5
Hendi Santika <hendisantika@gmail.com>
2021-05-10 05:04:27 +0700

    #6 - Add KafkaController

---
+++ src/main/java/com/hendisantika/springbootkafkaproducermysql/controller/KafkaController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootkafkaproducermysql.controller;
+
+import com.hendisantika.springbootkafkaproducermysql.service.KafkaConsumer;
+import com.hendisantika.springbootkafkaproducermysql.service.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-kafka-producer-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/05/21
+ * Time: 05.03
+ */
+@RestController
+public class KafkaController {
+    @Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+}
